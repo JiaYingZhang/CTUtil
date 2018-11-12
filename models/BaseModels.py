29502e5c660ca7d5c54f1479be4f9eb89e7354d4
JiaYingZhang <zhangjiaying121@163.com>
2018-11-12 22:35:12 +0800

    update basemodel field

--- models/BaseModels.py
+++ models/BaseModels.py
@@ -5,7 +5,7 @@ class BaseField(models.Model):
 
     createtime = models.DateTimeField('创建时间', auto_now_add=True)
     modifytime = models.DateTimeField('修改时间', auto_now=True)
-    remark = models.CharField('备注', max_length=32)
+    remark = models.CharField('备注', max_length=32, null=True, default=None, blank=True)
 
     class Meta:
         abstract = True
