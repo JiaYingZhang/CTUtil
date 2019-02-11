c4f11a1aed57802cacbb25350bb5848c5fdb62aa
JiaYingZhang <zhangjiaying121@163.com>
2019-02-11 11:04:49 +0800

    update reponse

--- Response/__init__.py
+++ Response/__init__.py
@@ -1,4 +1,3 @@
-from CTUtil.Response.response import resp_error_json
-from CTUtil.Response.response import resp_to_json
+from CTUtil.Response.response import resp_error_json, resp_to_json, resp_to_file
 
-__all__ = ['resp_error_json', 'resp_to_json']
+__all__ = ['resp_error_json', 'resp_to_json', 'resp_to_file']
