2346cca490e063724e04d42a42510f2dd8f5549a
JiaYingZhang <zhangjiaying121@163.com>
2019-08-19 14:04:24 +0800

    update form

---
+++ forms/__init__.py
@@ -0,0 +1,3 @@
+from CTUtil.forms.field import __all__ as fields_all
+from CTUtil.forms.valid_func import __all__ as valid_all
+__all__ = fields_all + valid_all
---
+++ forms/field.py
@@ -0,0 +1,79 @@
+from typing import Callable, Any, Optional, Tuple, Dict, Type
+from CTUtil.util import jstimestamp_to_datetime
+
+__all__ = ['Field', 'CharField', 'IntField', 'JsTimeStampField', 'Form']
+
+
+class Field(object):
+    def __init__(
+            self,
+            backend: str,
+            valid_func: Optional[Callable[[Any], Tuple[Any, str]]] = None):
+        self.backend = backend
+        self.valid_func = valid_func
+
+    def valid(self, value: Any) -> Tuple[Any, str]:
+        if self.valid_func:
+            return self.valid_func(value)
+        return value, ''
+
+
+class CharField(Field):
+    def valid(self, value):
+        value, err = super().valid(str(value))
+        if err != '':
+            return value, err
+        return value, err
+
+
+class IntField(Field):
+    def valid(self, value):
+        value, err = super().valid(int(value))
+        if err != '':
+            return value, err
+        return value, err
+
+
+class JsTimeStampField(Field):
+    def valid(self, value):
+        value, err = super().valid(value)
+        if err != '':
+            return value, err
+        return jstimestamp_to_datetime(int(value)), err
+
+
+class FormMeta(type):
+    def __new__(cls, clsname: str, bases: Tuple[object],
+                clsdict: Dict[str, Any]):
+        fields: Dict[str, Field] = {}
+        for key, filed in clsdict.items():
+            if isinstance(filed, Field):
+                fields[key] = filed
+        for key in fields.keys():
+            del clsdict[key]
+        clsdict['fields'] = fields
+        return super().__new__(cls, clsname, bases, clsdict)
+
+
+class Form(metaclass=FormMeta):
+    def __init__(self, data: Dict[str, Any]):
+        self.data = data
+        self.error: Dict[str, str] = {}
+        self.backend: Dict[str, Any] = {}
+
+    def is_valid(self) -> bool:
+        self.error.clear()
+        self.backend.clear()
+        self.fields: dict
+        isvalid: bool = True
+        for name, value in self.data.items():
+            field: Type[Field] = self.fields.setdefault(name)
+            if not field:
+                continue
+            value, err = field.valid(value)
+            if err != '':
+                self.error[name] = err
+                isvalid = False
+            else:
+                self.backend[field.backend] = value
+        return isvalid
