7c6a341fc955f4d9b07c11f02ed897047abafe25
JiaYingZhang <zhangjiaying121@163.com>
2019-09-12 14:19:28 +0800

    update middle module

--- middleware/__init__.py
+++ middleware/__init__.py
@@ -1,4 +1,3 @@
 from CTUtil.middleware.exception import ProcessException
-from CTUtil.Cache import CachesMiddleware
 
-__all__ = ('CachesMiddleware', 'ProcessException')
+__all__ = ('ProcessException')
