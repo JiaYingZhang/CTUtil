41a060692d72a3e75c39f7b1f218623f7dd82d61
JiaYingZhang <zhangjiaying121@163.com>
2019-10-16 11:59:24 +0800

    update email module

--- email/__init__.py
+++ email/__init__.py
@@ -1,20 +1,20 @@
-from CTUtil.email.util import CingTaEmail, BaseEmail
+from CTUtil.email.util import CingTaEmail, BaseEmail, EmailTemplate
 from django.core.mail import send_mail
 from typing import List, Union, Type
-from CTUtil.types import EmailTypes, FuncCallBack
+from CTUtil.types import FuncCallBack
 
-__all__ = ('CingTaEmail', 'BaseEmail', 'send_mail', 'EmailTypes',
+__all__ = ('CingTaEmail', 'BaseEmail', 'send_mail', 'EmailTemplate'
            'FuncCallBack')
 
 
-def send_cingta_email(
-        title: str,
-        to_email: List[str],
-        model: Type[EmailTypes]=EmailTypes.DEMAND,
-        msg: Union[None, str]=None,
-        from_email_name: str='cingta',
-        **kwargs) -> Type[FuncCallBack]:
-    mail: CingTaEmail = CingTaEmail(title, to_email, model, msg, from_email_name, **kwargs)
+def send_cingta_email(title: str,
+                      to_email: List[str],
+                      model: Type[EmailTemplateEmailTemplate],
+                      msg: Union[None, str] = None,
+                      from_email_name: str = 'cingta',
+                      **kwargs) -> Type[FuncCallBack]:
+    mail: CingTaEmail = CingTaEmail(title, to_email, model, msg,
+                                    from_email_name, **kwargs)
     try:
         send_mail(**mail.email_msg)
         return FuncCallBack.SUCCESS
