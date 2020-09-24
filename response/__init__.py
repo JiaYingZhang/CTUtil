3df0c01a738a46fc5b7d43482e93cc984ea24214
unknown <zhangjiaying121@163.com>
2020-09-24 19:39:04 +0800

    update __init__

--- response/__init__.py
+++ response/__init__.py
@@ -1,4 +1,4 @@
-from CTUtil.response.response import resp_error_json, resp_to_json, resp_to_file
+from CTUtil.response.response import resp_error_json, resp_to_json, resp_to_file, resp_to_stream
 from CTUtil.types import ResponseStates, HTTPResponseStates
 
 __all__ = [
@@ -7,4 +7,5 @@ __all__ = [
     'resp_to_file',
     'ResponseStates',
     'HTTPResponseStates',
+    'resp_to_stream',
 ]
